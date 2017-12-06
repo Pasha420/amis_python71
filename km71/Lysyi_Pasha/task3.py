b550041c3eacea56f01f9d607dfed3ed940996ad
Pasha420 <31793429+Pasha420@users.noreply.github.com>
2017-12-06 21:55:29 +0200

    Delete task3.py

--- km71/Lysyi_Pasha/task3.py
+++
@@ -1,9 +0,0 @@
-def backward(list):#оголошення функції
-    if len(list) < 2:
-        return list
-    else:
-        x = len(list)//2
-        return backward(list[x:])+backward(list[:x]) #використання рекурсії для обернення ліста
-
-
-print(backward(input("please write a list ").split())) ##введення ліста та їх сплітування і визивання функції від ліста
