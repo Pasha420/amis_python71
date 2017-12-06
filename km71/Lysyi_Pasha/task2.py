3004d086f19addaaa771d18e50b20ab25bab03b0
Pasha420 <31793429+Pasha420@users.noreply.github.com>
2017-12-06 21:55:21 +0200

    Delete task2.py

--- km71/Lysyi_Pasha/task2.py
+++
@@ -1,7 +0,0 @@
-def Min(list):#оголошення функції
-    if len(list) == 1:
-        return list[0]
-    else:
-        return min(Min(list[:-1]), list[-1]) #знаходження мінімального елменту за допомогою рекурсії 
-
-print(Min(input("please write a list ").split())) #введення ліста та їх сплітування і визивання функції від ліста
